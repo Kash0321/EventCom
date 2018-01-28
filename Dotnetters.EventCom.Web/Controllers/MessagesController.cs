e3f8d4d7dbdab627bc5a570bac51cef024c0e6eb
Kash0321 <castillo.sergio@gmail.com>
2018-01-28 09:20:05 +0100

    Método Web API para el envío de mensajes a través de signalr funcionando

---
+++ Dotnetters.EventCom.Web/Controllers/MessagesController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Dotnetters.EventCom.Web.Hubs;
+using Dotnetters.EventCom.Web.Models;
+using Microsoft.AspNet.SignalR.Client;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Dotnetters.EventCom.Web.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Messages")]
+    public class MessagesController : Controller
+    {
+        IHubContext<MessagingHub> MessagingHubContext { get; set; }
+
+        /// <summary>
+        /// Inicializa una instancia de <see cref="MessagesController"/>
+        /// </summary>
+        /// <param name="messagingHubContext"></param>
+        public MessagesController(IHubContext<MessagingHub> messagingHubContext)
+        {
+            MessagingHubContext = messagingHubContext;
+        }
+
+        // POST: api/Messages
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody]MessagePost messageData)
+        {
+            await MessagingHubContext.Clients.All.InvokeAsync("Send", messageData.User, messageData.Message);
+
+            return Ok(messageData);
+        }
+    }
+}
---
+++ Dotnetters.EventCom.Web/Models/MessagePost.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Dotnetters.EventCom.Web.Models
+{
+    /// <summary>
+    /// Datos de envío de un mensaje desde un dispositivo remoto
+    /// </summary>
+    public class MessagePost
+    {
+        /// <summary>
+        /// Nombre del usuario que envía el mensaje
+        /// </summary>
+        public string User { get; set; }
+
+        /// <summary>
+        /// Mensaje enviado por el usuario
+        /// </summary>
+        public string Message { get; set; }
+    }
+}
