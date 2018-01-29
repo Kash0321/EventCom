97a841ad56bb3ba2fbcb6004fd327cd0e24161ef
Kash0321 <castillo.sergio@gmail.com>
2018-01-29 15:56:04 +0100

    Diseño del visualizador web

--- Dotnetters.EventCom.Web/ClientApp/app/components/home/home.component.ts
+++ Dotnetters.EventCom.Web/ClientApp/app/components/home/home.component.ts
@@ -22,14 +22,16 @@ export class HomeComponent implements OnInit {
 
     public sendMessage(): void {
         this._hubConnection.invoke('Send', this.tuser, this.message);
-        this.messages.unshift({ 'user': this.tuser, 'message': this.message, 'sentorreceived': 'Sent' });
+        //this.messages.unshift({ 'user': this.tuser, 'message': this.message, 'sentorreceived': 'Sent' });
     }
 
     ngOnInit() {
         this._hubConnection = new HubConnection('/messaging');
 
         this._hubConnection.on('Send', (user: string, message: string) => {
-            this.messages.unshift({ 'user': user, 'message': message, 'sentorreceived': 'Received' });
+            let d = new Date();
+            let dStr = d.getDate() + '/' + d.getMonth() + 1 + '/' + d.getFullYear() + ' ' + d.getHours() + ':' + d.getMinutes();
+            this.messages.unshift({ 'user': user, 'message': message, 'sentorreceived': 'Received', 'date': dStr  });
         });
 
         this._hubConnection.start()
